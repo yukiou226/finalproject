dfdb433224b541bca1141e15c1e2a411e7bf17d2
Hualin Wu <hualinwu@microsoft.com>
2025-06-30 17:35:29 -0700

    Fix registration functionality and add debug mode

--- server/models/User.js
+++ server/models/User.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
@@ -12,14 +16,6 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-});
-
-// Hash password before save
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
